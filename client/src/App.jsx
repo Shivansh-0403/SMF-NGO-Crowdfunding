77b8d811baae7588ba82c315ef7110e2186624ac
Shivansh Srivastava <shivansh.srivastava0403@gmail.com>
2024-08-25 23:33:23 +0530

    Project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  )
+}
+
+export default App
