3c37afbec499793010a276d8ca9f5cd438101eb2
Shivansh Srivastava <shivansh.srivastava0403@gmail.com>
2024-09-01 23:33:25 +0530

    fixing problems

--- server/index.js
+++ server/index.js
@@ -22,11 +22,11 @@ dotenv.config({
     path: './.env'
 })
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// });
 
 const connectDB = async () => {
     try {
@@ -49,5 +49,5 @@ connectDB()
     console.log("MONGO db connection failed !!! ", err);
 })
 
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.routes.js'
 app.use("/api/user", userRouter)
